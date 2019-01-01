5c2d4cc7011bfc7f37bce8b7cc7f965d06818faa
Geoffrey Kasebacher <gkasenbacher@gmail.com>
2019-01-01 15:35:19 -0800

    new data
    added dependant variable!

--- Code/main.py
+++ Code/main.py
@@ -6,9 +6,24 @@ df = pd.get_dummies(df, columns=["gsector", "ggroup","gind", "gsector", "gsubind
 print(df.describe)
 df = df.drop("iid",axis=1)
 
-## This code splits the dataset above into datasets sorted by company
+
+## Creates new dependant variables for direction of stock movement (we want to forecast the price direction)
+## We can use the same code for other time periods of course
+dependantclose = df['prccd'].diff()
+newdf = pd.DataFrame(dependantclose)
+newdf.loc[newdf['prccd'] < 0, "direction"] = 0 
+newdf.loc[newdf['prccd'] > 0, "direction"] = 1
+df = pd.concat([df, newdf['direction']], axis=1, sort=False)
+
+
+## This code splits the dataset above into datasets sorted by company and saves them
 stocklist = pd.unique(df['tic'])
 for i in stocklist: 
     df1 = df[df['tic'] == i]
-    df1.to_csv(i)
+    # df1.to_csv(i)
 
+## Loading company level Data (access all company strings in the list "stocklist")
+## Also important to drop the first row! I'll explain why
+
+df1 = pd.read_csv("..//"+"Data//"+"Stockdata//"+"GOOGL")
+df1.drop(df.index[0])
