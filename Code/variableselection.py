925013cd924d5eba1742bb50ed8ca3e816651df3
Geoffrey Kasebacher <gkasenbacher@gmail.com>
2019-01-04 20:22:15 -0800

    issues + varselection

---
+++ Code/variableselection.py
@@ -0,0 +1,38 @@
+## variables selection methods here
+## Was thinking of using CV Lasso and PCA, open to other suggestions
+
+import numpy as np
+import pandas as pd
+
+# loading random company
+df1 = pd.read_csv("..//"+"Data//"+"Stockdata//"+"AMAT.csv")
+df1.drop(df1.index[0])
+
+## Bunch of transformations also defining covariates and dependant variables
+#df1 = pd.get_dummies(df1, columns=["gsector", "ggroup","gind", "gsector", "gsubind", "gvkey","tic", "gvkey", "spcsrc", "incorp", "idbflag"])
+#df1 = df1.reset_index()
+df1 = df1.drop(["capgn","spcseccd","incorp","spcsrc", "idbflag", "tic", "prccd", "prchd", "prcld", "prcod"],axis=1)
+df1 = df1.dropna()
+y = df1['direction']
+X = df1.drop(["direction"], axis=1)
+## Making test splits: Test size is how much of the data we use as training data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10)
+
+## Training the Data with decision trees and random forest
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression(penalty='l1')
+
+classifier.fit(X_train, y_train)
+
+## Making the actual prediciton
+y_pred = classifier.predict(X_test)
+
+## Some elementary diagnostics, pretty good results!
+from sklearn.metrics import classification_report, confusion_matrix
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
