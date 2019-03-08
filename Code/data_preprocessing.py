899a7f9efe53b442da9ca322d2f609964d30f3e9
Kaan Yolsever <kaanyolsever@gmail.com>
2019-03-08 12:36:21 -0800

    update

--- Code/data_preprocessing.py
+++ Code/data_preprocessing.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("..//"+"Data//"+"initial_dataset.csv", engine='python')
-df = pd.get_dummies(df, columns=["gsector", "ggroup","gind", "gsector", "gsubind", "gvkey", "gvkey"])
-df = df.drop("iid",axis=1)
+# df = pd.get_dummies(df, columns=["gsector", "ggroup","gind", "gsector", "gsubind", "gvkey", "gvkey"])
+# df = df.drop("iid",axis=1)
 
 
 ## Creates new dependant variables for direction of stock movement (we want to forecast the price direction)
 ## We can use the same code for other time periods of course
-dependantclose = df['prccd'].diff()
-newdf = pd.DataFrame(dependantclose)
-newdf.loc[newdf['prccd'] < 0, "direction"] = 0
-newdf.loc[newdf['prccd'] > 0, "direction"] = 1
-df = pd.concat([df, newdf['direction']], axis=1, sort=False)
+# dependantclose = df['prccd'].diff()
+# newdf = pd.DataFrame(dependantclose)
+# newdf.loc[newdf['prccd'] < 0, "direction"] = 0
+# newdf.loc[newdf['prccd'] > 0, "direction"] = 1
+# df = pd.concat([df, newdf['direction']], axis=1, sort=False)
 
 
 ## This code splits the dataset above into datasets sorted by company and saves them
